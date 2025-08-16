713ee65894768cca6e5c96f4b7c66add1a4af0e0
Allan.J <langatjayallan@gmail.com>
2025-08-16 17:40:42 +0300

    core report model created

--- waste/models.py
+++ waste/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class CollectionRequest(models.Model):
+    requester = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="requests/", null=True, blank=True)
+    location_text = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, default="PENDING")
+    created_at = models.DateTimeField(auto_now_add=True)
