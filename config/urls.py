c73e36cc2e4ac32b4cb60f7100df95f4912334c3
Allan.J <langatjayallan@gmail.com>
2025-08-29 18:26:07 +0300

    urls.py updated

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
+#From apps.collection
 router.register(r"collection-requests", CollectionRequestViewSet, basename="collectionrequest")
 # register other viewsets similarly...
 
@@ -36,7 +37,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
-
+#From apps.collection
 router.register(r"collection-requests", CollectionRequestViewSet, basename="collectionrequest")
 
 from django.conf import settings
