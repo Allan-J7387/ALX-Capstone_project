3085e2118bac9eacdf07018d1d01cd7264d0e6a3
Allan.J <langatjayallan@gmail.com>
2025-08-29 12:35:01 +0300

    models.py updated

--- apps/waste/models.py
+++ apps/waste/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-class CollectionRequest(models.Model):
-    requester = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    description = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="requests/", null=True, blank=True)
-    location_text = models.CharField(max_length=255)
-    status = models.CharField(max_length=20, default="PENDING")
-    created_at = models.DateTimeField(auto_now_add=True)
-
 class WasteType(models.Model):
     name = models.CharField(max_length=80)
     description = models.TextField(blank=True)
